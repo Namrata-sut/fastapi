20077cad119189d7f2588bfbf7af55b9381fa4ae
Namrata.Sutar <namrata.sutar@annalect.com>
2024-10-01 20:10:52 +0530

    Added input and output schemas for all routes. Updated input and output schemas for all routes.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, List
 
 from fastapi import FastAPI, status, HTTPException, Query
 from pydantic import BaseModel, Field
@@ -8,6 +8,13 @@ from sqlalchemy.orm import mapper
 
 from database import SessionLocal, engine
 import models
+
+from schema import (
+    PokemonPostPutInputSchema,
+    PokemonPatchInputSchema,
+    PokemonGetOutputSchema,
+    PokemonPostPatchPutOutputSchema,
+)
 
 app = FastAPI()
 db = SessionLocal()
@@ -34,17 +41,29 @@ def getInfo():
     return {"message": "Server is running"}
 
 
-@app.get('/pokemon/{pokemon_id}', response_model=Pokemon, status_code=status.HTTP_200_OK)
+@app.get(
+    "/pokemon/{pokemon_id}",
+    response_model=PokemonGetOutputSchema,
+    status_code=status.HTTP_200_OK,
+)
 def get_Pokemon_By_Id(pokemon_id: int):
-    getSinglePokemon = db.query(models.PokemonData).filter(models.PokemonData.id == pokemon_id).first()
+    getSinglePokemon = (
+        db.query(models.PokemonData).filter(models.PokemonData.id == pokemon_id).first()
+    )
     if getSinglePokemon is not None:
         return getSinglePokemon
     else:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Pokemon not found..")
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Pokemon not found.."
+        )
 
 
-@app.post('/pokemon', response_model=Pokemon, status_code=status.HTTP_201_CREATED)
-def add_Pokemon(pokemon: Pokemon):
+@app.post(
+    "/pokemon",
+    response_model=PokemonPostPatchPutOutputSchema,
+    status_code=status.HTTP_201_CREATED,
+)
+def add_Pokemon(pokemon: PokemonPostPutInputSchema):
     # max_id = db.query(func.max(models.PokemonData.id)).scalar()
     # new_id = (max_id or 0) + 1
     newPokemon = models.PokemonData(
@@ -72,11 +91,17 @@ def add_Pokemon(pokemon: Pokemon):
     return newPokemon
 
 
-@app.put('/pokemon/{pokemon_id}', response_model=Pokemon, status_code=status.HTTP_202_ACCEPTED)
-def update_Pokemon(pokemon_id: int, pokemon: Pokemon):
-    find_pokemon = db.query(models.PokemonData).filter(models.PokemonData.id == pokemon_id).first()
+@app.put(
+    "/pokemon/{pokemon_id}",
+    response_model=PokemonPostPatchPutOutputSchema,
+    status_code=status.HTTP_202_ACCEPTED,
+)
+def update_Pokemon(pokemon_id: int, pokemon: PokemonPostPutInputSchema):
+    find_pokemon = (
+        db.query(models.PokemonData).filter(models.PokemonData.id == pokemon_id).first()
+    )
     if find_pokemon is not None:
-        find_pokemon.id = pokemon.id
+        # find_pokemon.id = pokemon.id
         find_pokemon.name = pokemon.name
         find_pokemon.type_1 = pokemon.type_1
         find_pokemon.type_2 = pokemon.type_2
@@ -92,39 +117,55 @@ def update_Pokemon(pokemon_id: int, pokemon: Pokemon):
         db.commit()
         return find_pokemon
     else:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Pokemon with this id is not exist..")
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Pokemon with this id is not exist..",
+        )
 
 
-@app.patch("/pokemon/{pokemon_id}", response_model=Pokemon)
-def update_Pokemon_Patch(pokemon_id: str, pokemon: Pokemon):
-    find_pokemon = db.query(models.PokemonData).filter(models.PokemonData.id == pokemon_id).first()
+@app.patch("/pokemon/{pokemon_id}", response_model=PokemonPostPatchPutOutputSchema)
+def update_Pokemon_Patch(pokemon_id: str, pokemon: PokemonPatchInputSchema):
+    find_pokemon = (
+        db.query(models.PokemonData).filter(models.PokemonData.id == pokemon_id).first()
+    )
     if not find_pokemon:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"pokemon with id {pokemon_id} doesn't exist...")
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"pokemon with id {pokemon_id} doesn't exist...",
+        )
     for key, value in pokemon.dict(exclude_unset=True).items():
         setattr(find_pokemon, key, value)
     return find_pokemon
 
 
-@app.delete('/pokemon/{pokemon_id}', response_model=Pokemon, status_code=200)
+@app.delete("/pokemon/{pokemon_id}", response_model=Pokemon, status_code=200)
 def delete_Pokemon(pokemon_id):
     if not pokemon_id.isdigit():
         raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST, detail="Pokemon ID must be a valid integer."
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Pokemon ID must be a valid integer.",
         )
 
-    find_pokemon = db.query(models.PokemonData).filter(models.PokemonData.id == pokemon_id).first()
+    find_pokemon = (
+        db.query(models.PokemonData).filter(models.PokemonData.id == pokemon_id).first()
+    )
     if find_pokemon is not None:
         db.delete(find_pokemon)
         db.commit()
-        raise HTTPException(status_code=status.HTTP_200_OK, detail="Pokemon with this id is deleted successfully..")
+        raise HTTPException(
+            status_code=status.HTTP_200_OK,
+            detail="Pokemon with this id is deleted successfully..",
+        )
     else:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Pokemon with this id is either alreday deleted or not found..")
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Pokemon with this id is either alreday deleted or not found..",
+        )
 
 
 @app.post("/pokemon/fetch_and_store/")
 def fetch_and_store():
-    response = requests.get('https://coralvanda.github.io/pokemon_data.json')
+    response = requests.get("https://coralvanda.github.io/pokemon_data.json")
     data = response.json()
     # print(f"{data}- data fetched")
 
@@ -150,7 +191,7 @@ def fetch_and_store():
             "sp_def": pokemon["Sp. Def"],
             "speed": pokemon["Speed"],
             "generation": pokemon["Generation"],
-            "legendary": pokemon["Legendary"]
+            "legendary": pokemon["Legendary"],
         }
         for pokemon in data
     ]
@@ -169,12 +210,18 @@ def fetch_and_store():
     return {"message": "Data successfuly stored in the database"}
 
 
-@app.get("/pokemon/")
-def get_pokemon(sort: str = Query("asc", description="Sort order: 'asc' or 'desc'"),
-                keyword: Optional[str] = Query(None, description="Search keyword"),
-                col: str = Query("name", description="Column to search in, default 'name'"),
-                limit: int = Query(10, description="Results per page, default 10", le=100),
-                page: int = Query(1, description="Page number")):
+@app.get(
+    "/pokemon/",
+    response_model=List[PokemonGetOutputSchema],
+    status_code=status.HTTP_200_OK,
+)
+def get_pokemon(
+    sort: str = Query("asc", description="Sort order: 'asc' or 'desc'"),
+    keyword: Optional[str] = Query(None, description="Search keyword"),
+    col: str = Query("name", description="Column to search in, default 'name'"),
+    limit: int = Query(10, description="Results per page, default 10", le=100),
+    page: int = Query(1, description="Page number"),
+):
     mapper = inspect(models.PokemonData)
 
     # Verify column exists in the model
@@ -188,19 +235,33 @@ def get_pokemon(sort: str = Query("asc", description="Sort order: 'asc' or 'desc
     if keyword:
         if isinstance(column_type, Integer):
             if not keyword.isdigit():
-                raise HTTPException(status_code=400, detail=f"Keyword '{keyword}' is not valid for column '{col}' (expected int)..")
+                raise HTTPException(
+                    status_code=400,
+                    detail=f"Keyword '{keyword}' is not valid for column '{col}' (expected int)..",
+                )
             keyword = int(keyword)
         elif isinstance(column_type, Boolean):
             if keyword.lower() not in ["true", "false"]:
-                raise HTTPException(status_code=400, detail=f"Keyword '{keyword}' is not valid for column '{col}' (expected bool: 'true' or 'false')..")
+                raise HTTPException(
+                    status_code=400,
+                    detail=f"Keyword '{keyword}' is not valid for column '{col}' (expected bool: 'true' or 'false')..",
+                )
             keyword = keyword.lower() == "true"
         elif isinstance(column_type, String):
             if keyword.isdigit():
-                raise HTTPException(status_code=400, detail=f"Keyword '{keyword}' is not valid for column '{col}' (expected string, got numeric).")
+                raise HTTPException(
+                    status_code=400,
+                    detail=f"Keyword '{keyword}' is not valid for column '{col}' (expected string, got numeric).",
+                )
             elif keyword.lower() in ["true", "false"]:
-                raise HTTPException(status_code=400, detail=f"Keyword '{keyword}' is not valid for column '{col}' (expected string, got boolean-like value).")
+                raise HTTPException(
+                    status_code=400,
+                    detail=f"Keyword '{keyword}' is not valid for column '{col}' (expected string, got boolean-like value).",
+                )
         else:
-            raise HTTPException(status_code=400, detail=f"Unsupported data type for column '{col}'.")
+            raise HTTPException(
+                status_code=400, detail=f"Unsupported data type for column '{col}'."
+            )
 
     query = db.query(models.PokemonData)
 
@@ -217,7 +278,10 @@ def get_pokemon(sort: str = Query("asc", description="Sort order: 'asc' or 'desc
     elif sort == "desc":
         query = query.order_by(desc(models.PokemonData.id))
     else:
-        raise HTTPException(status_code=400, detail=f"Invalid input.. please choose ascending or descending")
+        raise HTTPException(
+            status_code=400,
+            detail=f"Invalid input.. please choose ascending or descending",
+        )
 
     # Pagination
     offset = (page - 1) * limit
