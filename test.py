4c63ec7c64e431741266c19ca201e8b607f4905e
Namrata.Sutar <namrata.sutar@annalect.com>
2024-10-11 14:53:12 +0530

    Updated test cases using the test database.

--- test.py
+++ test.py
@@ -1,193 +1,221 @@
 from fastapi.testclient import TestClient
+from sqlalchemy import create_engine, StaticPool
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Column, Integer, String, Boolean
+
+from database import get_db, Base
 from main import app
-from database import SessionLocal, engine
+
+TEST_DATABASE_URL = 'postgresql://postgres:gai3905@localhost/test_pokemon'
+
+# Create an engine and a session for the test database
+test_engine = create_engine(TEST_DATABASE_URL, poolclass=StaticPool)
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=test_engine)
 
 client = TestClient(app)
-db = SessionLocal()
 
 
 # Override the database dependency for testing
 def override_get_db():
+    db = TestingSessionLocal()
     try:
         yield db
     finally:
         db.close()
 
 
-app.dependency_overrides[db] = override_get_db
+# Override the dependency in the FastAPI app
+app.dependency_overrides[get_db] = override_get_db
 
 
-class Test_Pokemon:
+def create_all_tables():
+    """Setup for the tests: create the database tables."""
+    Base.metadata.create_all(bind=test_engine)
 
-    def test_main(self):
-        response = client.get("/test")
-        assert response.status_code == 200
-        assert response.json() == {"message": "Server is running"}
 
-    def test_get_pokemon_by_id(self):
-        response = client.get("/pokemon/10")
-        assert response.status_code == 200
-        assert response.json()["detail"] == "Pokemon found.."
+def drop_all_tables():
+    """Drp for the tests: drop the database tables."""
+    Base.metadata.drop_all(bind=test_engine)
 
-    def test_get_pokemon_by_id_not_found(self):
-        response = client.get("/pokemon/10009")
-        assert response.status_code == 404
-        assert response.json()["detail"] == "Pokemon not found.."
 
-    def test_get_pokemon_invalid_id(self):
-        response = client.get("/pokemon/invalid_id")
-        assert response.status_code == 422
+def test_route():
+    response = client.get("/test")
+    assert response.status_code == 200
+    assert response.json() == {'message': 'Server is running'}
 
-    def test_add_pokemon(self):
-        pokemon_data = {
-            "id": 0,
-            "name": "Test Pokémon",
-            "type_1": "Fire",
-            "type_2": "Grass",
-            "total": 100,
-            "hp": 10,
-            "attack": 20,
-            "defense": 30,
-            "sp_atk": 40,
+
+def test_create_pokemon():
+    """Add a sample Pokemon to the database for testing."""
+    response = client.post(
+        "/pokemon", json={
+            "name": "Pikachu",
+            "type_1": "Electric",
+            "type_2": None,
+            "total": 320,
+            "hp": 35,
+            "attack": 55,
+            "defense": 40,
+            "sp_atk": 50,
             "sp_def": 50,
-            "speed": 30,
+            "speed": 90,
             "generation": 1,
-            "legendary": False,
+            "legendary": False
         }
-        response = client.post("/pokemon", json=pokemon_data)
-        assert response.status_code == 201
-        assert response.json()["name"] == "Test Pokémon"
+    )
+    assert response.status_code == 201, response.text
+    data = response.json()
+    assert data["name"] == "Pikachu"
 
-    def test_update_pokemon(self):
-        updated_data = {
-            "id": 4019,
-            "name": "Updated Pokémon",
-            "type_1": "Water",
+
+def test_read_pokemon_by_existing_id():
+    response = client.get("/pokemon/1")
+    assert response.status_code == 200
+    data = response.json()
+    assert data
+
+
+def test_read_pokemon_by_non_existing_id():
+    response = client.get("/pokemon/1000")
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Pokemon not found."}
+
+
+def test_update_pokemon():
+    """Test updating an existing Pokemon in the database."""
+
+    # Create a sample Pokemon to update
+    response_create = client.post(
+        "/pokemon", json={
+            "name": "Pikachu",
+            "type_1": "Electric",
             "type_2": None,
-            "total": 150,
-            "hp": 20,
-            "attack": 30,
+            "total": 320,
+            "hp": 35,
+            "attack": 55,
             "defense": 40,
             "sp_atk": 50,
-            "sp_def": 60,
-            "speed": 70,
-            "generation": 2,
-            "legendary": True,
-        }
-
-        # Send a PUT request to the endpoint
-        response = client.put(f'/pokemon/{updated_data["id"]}', json=updated_data)
-
-        # Assert the response status code is 202
-        # assert response.status_code == 202
-
-        # Assert the response data matches the updated data
-        assert response.json() == updated_data
-
-    def test_update_non_existing_pokemon(self):
-        updated_data = {
-            "id": 40190,
-            "name": "Updated Pokémon",
-            "type_1": "Water",
-            "type_2": None,
-            "total": 150,
-            "hp": 20,
-            "attack": 30,
-            "defense": 40,
-            "sp_atk": 50,
-            "sp_def": 60,
-            "speed": 70,
-            "generation": 2,
-            "legendary": True,
-        }
-
-        # Send a PUT request to the endpoint
-        response = client.put(f'/pokemon/{updated_data["id"]}', json=updated_data)
-
-        assert response.status_code == 404
-        assert response.json()["detail"] == f"Pokemon with this id is not exist.."
-
-        # Assert the response data matches the updated data
-        # assert response.json() == updated_data
-
-    def test_update_pokemon_patch(self):
-        # Partial update data
-        pokemon_id = 900
-        updated_data = {
-            "id": pokemon_id,
-            "name": "Test",
-            "type_1": "Water",
-            "type_2": "Poison",
-            "total": 310,
-            "hp": 30,
-            "attack": 35,
-            "defense": 30,
-            "sp_atk": 100,
-            "sp_def": 35,
-            "speed": 80,
+            "sp_def": 50,
+            "speed": 90,
             "generation": 1,
-            "legendary": False,
+            "legendary": False
         }
+    )
 
-        # Send a PATCH request to the endpoint
-        response = client.patch(f"/pokemon/{pokemon_id}", json=updated_data)
+    assert response_create.status_code == 201
+    pokemon_id = response_create.json()["id"]
 
-        # assert response.status_code == 200
-        # assert response.json()["detail"] == f"pokemon with id {pokemon_id} updated..."
+    # Update the Pokemon
+    updated_data = {
+        "name": "Raichu",
+        "type_1": "Electric",
+        "type_2": None,
+        "total": 480,
+        "hp": 60,
+        "attack": 90,
+        "defense": 55,
+        "sp_atk": 90,
+        "sp_def": 80,
+        "speed": 110,
+        "generation": 1,
+        "legendary": False
+    }
 
-        # Assert the response data matches the updated data
-        assert response.json() == updated_data
+    response_update = client.put(f"/pokemon/{pokemon_id}", json=updated_data)
 
-    def test_update_nonexistent_pokemon_patch(self):
-        # Partial update data
-        pokemon_id = 4020
-        updated_data = {
-            "id": pokemon_id,
-            "name": "Test",
-            "type_1": "Water",
-            "type_2": "Poison",
-            "total": 310,
-            "hp": 30,
-            "attack": 35,
-            "defense": 30,
-            "sp_atk": 100,
-            "sp_def": 35,
-            "speed": 80,
-            "generation": 1,
-            "legendary": False,
-        }
+    assert response_update.status_code == 202
+    assert response_update.json()["name"] == "Raichu"
 
-        # Send a PATCH request to the endpoint
-        response = client.patch(f"/pokemon/{pokemon_id}", json=updated_data)
 
-        # Assert the response status code is 404
-        assert response.status_code == 404
-        assert response.json()["detail"] == f"pokemon with id {pokemon_id} doesn't exist..."
+def test_update_nonexistent_pokemon():
+    """Test updating a non-existent Pokemon, expecting 404 Not Found error."""
 
-    def test_delete_pokemon(self):
-        pokemon_id = 1601
-        # Send a DELETE request to the endpoint
-        response = client.delete(f"/pokemon/{pokemon_id}")
+    invalid_pokemon_id = 9999
 
-        # Assert the response status code is 200
-        assert response.status_code == 200
-        assert response.json()["detail"] == "Pokemon with this id is deleted successfully.."
+    updated_data = {
+        "name": "Raichu",
+        "type_1": "Electric",
+        "type_2": None,
+        "total": 480,
+        "hp": 60,
+        "attack": 90,
+        "defense": 55,
+        "sp_atk": 90,
+        "sp_def": 80,
+        "speed": 110,
+        "generation": 1,
+        "legendary": False
+    }
 
-    def test_delete_non_existing_pokemon(self):
-        pokemon_id = 901
-        # Send a DELETE request to the endpoint
-        response = client.delete(f"/pokemon/{pokemon_id}")
+    # update the Pokemon with an invalid ID
+    response = client.put(f"/pokemon/{invalid_pokemon_id}", json=updated_data)
 
-        # Assert the response status code is 404
-        assert response.status_code == 404
-        assert (
-                response.json()["detail"]
-                == "Pokemon with this id is either alreday deleted or not found.."
-        )
+    assert response.status_code == 404
+    assert response.json()["detail"] == "Pokemon not found."
 
-    def test_delete_pokemon_invalid_id(self):
-        pokemon_id = "abc"
-        response = client.delete(f"/pokemon/{pokemon_id}")
 
-        assert response.status_code == 400
-        assert response.json()["detail"] == "Pokemon ID must be a valid integer."
+def test_update_existing_pokemon_patch():
+    """Test updating an existing Pokemon should return the updated Pokemon data."""
+
+    pokemon_id = 1
+    updated_data = {
+        "name": "Pikachu Updated",
+        "type_1": "Electric",
+        "total": 320,
+        "hp": 35,
+        "attack": 55,
+        "defense": 40,
+        "sp_atk": 50,
+        "sp_def": 50,
+        "speed": 90,
+        "generation": 2,
+        "legendary": False
+    }
+
+    response = client.patch(f"/pokemon/{pokemon_id}", json=updated_data)
+
+    assert response.status_code == 200
+    data = response.json()
+    assert data["name"] == "Pikachu Updated"
+    assert data["type_1"] == "Electric"
+
+
+def test_update_non_existent_pokemon_patch():
+    """Test updating a Pokemon that does not exist should return 404."""
+
+    non_existent_pokemon_id = 9999
+
+    updated_data = {
+        "name": "Pikachu patch updated",
+        "type_1": "Electric",
+        "total": 320,
+        "hp": 35,
+        "attack": 55,
+        "defense": 40,
+        "sp_atk": 50,
+        "sp_def": 50,
+        "speed": 90,
+        "generation": 2,
+        "legendary": False
+    }
+
+    response = client.patch(f"/pokemon/{non_existent_pokemon_id}", json=updated_data)
+    assert response.status_code == 404
+    assert response.json()["detail"] == f"Pokemon with id {non_existent_pokemon_id} doesn't exist."
+
+
+def test_delete_existing_pokemon():
+    """Test deleting an existing Pokemon should return a success message."""
+
+    existing_pokemon_id = 1
+    response = client.delete(f"/pokemon/{existing_pokemon_id}")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Pokemon deleted successfully."}
+
+
+def test_delete_non_existent_pokemon():
+    """Test deleting a Pokemon that does not exist should return 404."""
+
+    non_existent_pokemon_id = 9999
+    response = client.delete(f"/pokemon/{non_existent_pokemon_id}")
+    assert response.status_code == 404
+    assert response.json()["detail"] == "Pokemon not found."
